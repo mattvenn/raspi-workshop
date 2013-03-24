5c5f35e9d79c8c31272cd4356db330c7647326fa
matt venn <matt@mattvenn.net>
2013-03-24 17:55:37 +0000

    made a servo_pin variable

--- servo/servo.py
+++ servo/servo.py
@@ -6,10 +6,11 @@ import time
 servo = PWM.Servo()
 
 #gpio pin 14 is pin 8
+servo_pin = 14
 
 while True:
 	# Set a 4000us (4ms) pulse every 20ms for GPIO 14:
-	servo.set_servo(14, 4000)
+	servo.set_servo(servo_pin, 4000)
 	time.sleep(0.5)
-	servo.set_servo(14, 2000)
+	servo.set_servo(servo_pin, 2000)
 	time.sleep(0.5)
