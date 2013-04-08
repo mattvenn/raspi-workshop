5d1d107192731f9211f6a665626a42aa43be6cfc
matthew venn <matt@mattvenn.net>
2013-04-08 13:45:56 +0100

    doc updates

--- flash_led/README.md
+++ flash_led/README.md
@@ -1,1 +1,7 @@
-add an LED and a resistor to pin 8 of the header (gpio 14)
+# flashing an LED
+
+Flashing an led is a good first example. The code is commented well, so be sure to read that.
+
+# Connections
+
+add an LED and a resistor in series to pin 8 of the header (gpio 14). There is an example circuit on the workshop resources.
--- flash_led/flash.py
+++ flash_led/flash.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python
+
+#import the library to control the GPIO pins
 import RPIO
+
+#import the time library
 import time
 
-#pin 8
+#gpio pin 14 is pin 8 on the raspberry pi (see the pin mapping on the workshop resources)
 led_pin = 14
+
+#setup the pin, and make it be off to start with
 RPIO.setup(led_pin, RPIO.OUT, initial=RPIO.LOW)
 
+#in a loop:
 while True:
     print True
+    #turn on the led
     RPIO.output(led_pin, True)
+    #wait for 0.5 seconds
     time.sleep(0.5)
     print False
+    #turn off the led
     RPIO.output(led_pin, False)
+    #sleep again
     time.sleep(0.5)
 
 
