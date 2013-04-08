31337bf3d2ee438f3c4f8660d30a099b9be08a4b
matthew venn <matt@mattvenn.net>
2013-04-08 13:49:44 +0100

    doc updates

--- pir/README.md
+++ pir/README.md
@@ -1,2 +1,10 @@
-see http://elinux.org/RPi_Low-level_peripherals#GPIO_hardware_hacking
-PIR from oomlout:
+# detecting a person's movement
+
+We're using a PIR (passive infra red sensor) from oomlout: http://www.oomlout.co.uk/motion-detector-pir-p-276.html
+When people move, the sensor's output pin goes high.
+
+# Connections
+
+* connect the PIR's ground pin to the pi's ground pin (6)
+* connect the PIR's + pin to the pi's 3.3v pin (1)
+* connect the PIR's output pin to pin 7 on the pi
--- pir/pir.py
+++ pir/pir.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
+"""
+author: matt venn
+"""
+
+#the library for the pin control
 import RPIO
 
-#see http://elinux.org/RPi_Low-level_peripherals#GPIO_hardware_hacking
 #gpio pin 4 is pin 7 on the p1 header
 pir_pin = 4
 
+#the interrupt function
 def int(gpio_id, value):
     print "something happened!"
 
+#setup the interrupt so it get's called with the output from the sensor goes high
 RPIO.add_interrupt_callback(pir_pin, int, edge='rising')
 RPIO.wait_for_interrupts()
-
