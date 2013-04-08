333e2a0a38f576101ddf0bf15106a3f87ebc800e
matt venn <matt@mattvenn.net>
2013-04-08 16:04:18 +0000

    working twitter demo

--- twitter/twitter.py
+++ twitter/twitter.py
@@ -1,15 +1,25 @@
+#!/usr/bin/python
+"""
+author: matt venn
+"""
+
+from datetime import datetime
+import os
 import tweepy
 from tweepy import OAuthHandler
 
-consumer_key= "khwzzasbbUHHxWwEn05uhw"
-consumer_secret= "7vOGEBYR7CYFoZFI4CYtLD506uLKNhzXMOPVeoLSRs"
+consumer_key= "fF86BdSdopE9FAES5UNgPw"
+consumer_secret= "n7G4K80kYQ6NDMQiYn3GY5Hyk82fF2So17Nl1UQdGWE"
 
-access_token= "19602951-MvhiSbEA56DzBLGlOOnYKjDHknijzYjYzZgZPsD1p"
-access_token_secret= "wk4EkPHwG2AFHzlvNpoJlxONNyPsOWocEM0olSGwwo"
+access_token= "1336977176-4CgpPJnJBx7kCRqnwLcRbXI3nLpHj44sp3r2bXy"
+access_token_secret= "5rLNvZm3JZdkx0K1Jx9jgsqMG6MmGLAQmPdJ7ChtzA"
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
-print "name",  api.me().name
+print "my twitter name:",  api.me().name
+hostname = os.uname()[1]
+time = datetime.now()
 
-api.update_status("Tweepy")
+api.update_status("the time on %s is %s" % ( hostname, time ))
