91b10bf87f44676bab3b2b2686556ea6ffe5107d
matt venn <matt@mattvenn.net>
2013-04-11 15:52:40 +0000

    suppress warnings

--- stepper/stepper.py
+++ stepper/stepper.py
@@ -6,12 +6,12 @@ author: matt venn"
 #import the RPIO library
 import RPIO
 import time
+RPIO.setwarnings(False)
 
 #gpio pin 4 is pin 7 on the p1 header
 step_pin = 4
 #gpio pin 17 is pin 11 on the p1 header
 dir_pin = 17
-
 #setup the pins
 RPIO.setup(step_pin, RPIO.OUT, initial=RPIO.LOW)
 RPIO.setup(dir_pin, RPIO.OUT, initial=RPIO.LOW)
@@ -20,6 +20,7 @@ RPIO.setup(dir_pin, RPIO.OUT, initial=RPIO.LOW)
 dir = False
 
 #then in a loop, step the motor forwards and backwards
+
 while True:
     print dir
     #move 50 steps
