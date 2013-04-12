84fc76e2791c80396f02fa8a93105ed39ad7538a
matt venn <matt@mattvenn.net>
2013-04-12 14:40:26 +0000

    allow some useful args to be passed

--- camera/webcam.py
+++ camera/webcam.py
@@ -7,11 +7,10 @@ author: matt venn
 import os
 
 #this function takes a photo using the fswebcam command
-def capture_image():
+def capture_image(filename="frame.jpg"):
     #this can do banners and timestamps
-    output = 'frame.jpg'
-    command = "fswebcam  --no-banner -r 800x600 -d /dev/video0 %s" % output
-    #command = "fswebcam --set brightness=50%% --no-banner -r 800x600 -d /dev/video0 %s" % output
+    command = "fswebcam  --no-banner -r 800x600 -d /dev/video0 %s" % filename
+    #command = "fswebcam --set brightness=50%% --no-banner -r 800x600 -d /dev/video0 %s" % filename
     status = os.system(command)
     return status
 
