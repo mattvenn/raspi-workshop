262bce94d606abea79fdfcd11c0197c7d8a234a0
matt venn <matt@mattvenn.net>
2013-04-15 14:19:25 +0000

    moved the read to a function for easy importing

--- barometer/bmp085.py
+++ barometer/bmp085.py
@@ -8,10 +8,15 @@ from Adafruit_BMP085 import BMP085
 #initialise the sensor
 bmp = BMP085(0x77)
 
-temp = bmp.readTemperature()
-pressure = bmp.readPressure()
-altitude = bmp.readAltitude()
+def read_sensor():
+	temp = bmp.readTemperature()
+	pressure = bmp.readPressure()
+	altitude = bmp.readAltitude()
+	return( temp,pressure,altitude )
 
-print "Temperature: %.2f C" % temp
-print "Pressure:    %.2f hPa" % (pressure / 100.0)
-print "Altitude:    %.2f" % altitude
+#if this script is run from the command line...
+if __name__=="__main__":  
+	(temp,pressure,altitude)=read_sensor()
+	print "Temperature: %.2f C" % temp
+	print "Pressure:    %.2f hPa" % (pressure / 100.0)
+	print "Altitude:    %.2f" % altitude
