6c78603c6447c419213c3a0314384dfcf2130545
matt venn <matt@mattvenn.net>
2013-04-23 15:48:21 +0000

    fixed button bug

--- button/button_simple.py
+++ button/button_simple.py
@@ -8,8 +8,9 @@ import RPIO
 #gpio pin 4 is pin 7 on the p1 header
 button_pin = 4
 
-GPIO.setup(button_pin,GPIO.IN)
+#set the pin to be high to start, low when pressed
+RPIO.setup(button_pin,RPIO.IN, pull_up_down=RPIO.PUD_UP)
 
 while True:
-    if (GPIO.input(button_pin)):
-        print("Button Pressed")
+    if RPIO.input(button_pin) == False:
+	print "button pressed"
