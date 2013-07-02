227ca2c184e8307fa3676274efdb45d44663b840
matthew venn <matt@mattvenn.net>
2013-07-02 15:31:46 +0100

    added networking

--- configuration/update.sh
+++ configuration/update.sh
@@ -8,3 +8,10 @@ git clone git@github.com:mattvenn/raspi-workshop.git
 
 #install vnc
 sudo apt-get install tightvncserver
+
+sudo echo 'nameserver 8.8.8.8' > /etc/resolv.conf
+
+sudo echo 'iface eth0 inet static
+address 192.168.0.15
+netmask 255.255.255.0
+gateway 192.168.0.1' >> /etc/network/interfaces'
