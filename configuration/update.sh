6f87c8f5820f070453366fadda629ea9ce7c5f16
matthew venn <matt@mattvenn.net>
2013-07-02 16:51:38 +0100

    got rid of static ip stuff

--- configuration/update.sh
+++ configuration/update.sh
@@ -10,10 +10,3 @@ sudo pip install RPi.GPIO --upgrade
 
 #install vnc
 sudo apt-get install tightvncserver
-
-sudo sh -c "echo 'nameserver 8.8.8.8' > /etc/resolv.conf"
-
-sudo sh -c "echo 'iface eth0 inet static
-address 192.168.0.15
-netmask 255.255.255.0
-gateway 192.168.0.1' >> /etc/network/interfaces"
