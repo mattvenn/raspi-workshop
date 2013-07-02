f6e8f6a076e744c57e6464425d0e1748c5fd11f9
matt venn <matt@mattvenn.net>
2013-07-02 12:22:20 +0000

    switched to gpio lib

--- stepper/stepper.py
+++ stepper/stepper.py
@@ -2,29 +2,29 @@
 author: matt venn"
 """
 
-#import the RPIO library
-import RPIO
+#import the GPIO library
+import RPi.GPIO as GPIO
 import time
-RPIO.setwarnings(False)
-RPIO.setmode(RPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 
 step_pin = 7
 dir_pin = 11
 #setup the pins
-RPIO.setup(step_pin, RPIO.OUT, initial=RPIO.LOW)
-RPIO.setup(dir_pin, RPIO.OUT, initial=RPIO.LOW)
+GPIO.setup(step_pin, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(dir_pin, GPIO.OUT, initial=GPIO.LOW)
 
 #a variable to know if we should go forwards or backwards
 dir = False
 
 def step(dir=False,step_time=0.01):
     #set the direction
-    RPIO.output(dir_pin, dir)
+    GPIO.output(dir_pin, dir)
 
     #take a step
-    RPIO.output(step_pin, True)
+    GPIO.output(step_pin, True)
     time.sleep(step_time)
-    RPIO.output(step_pin, False)
+    GPIO.output(step_pin, False)
     time.sleep(step_time)
 
 
