9f732bd28ccadf906332be2b8781af075ee39f45
Matt Venn <matt@mattvenn.net>
2013-07-03 16:20:01 +0100

    button.py

--- button/button.py
+++ button/button.py
@@ -3,14 +3,15 @@ author: matt venn
 """
 
 #import the library
-import RPIO
-RPIO.setmode(RPIO.BOARD)
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 
-button_pin = 7
+button_pin = 8
 
 #set the pin to be high to start, low when pressed
-RPIO.setup(button_pin,RPIO.IN, pull_up_down=RPIO.PUD_UP)
+RPIO.setup(button_pin,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    if RPIO.input(button_pin) == False:
+    if GPIO.input(button_pin) == False:
 	print "button pressed"
