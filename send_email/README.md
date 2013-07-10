0c7fa56faf8b7e3bcf3c3bfba26c841f91c72a52
Matt Venn <matt@mattvenn.net>
2013-07-10 16:22:40 +0100

    sending email demo

---
+++ send_email/README.md
@@ -0,0 +1,8 @@
+# sending an email
+
+We can use a python library called smtplib to send an email via gmail. You can read more about the library here:
+
+    http://docs.python.org/2/library/smtplib.html
+
+To use the demo, you'll need to change the gmail username and password at the top, and then change the recipient's email address at the bottom of the file.
+
---
+++ send_email/send_email.py
@@ -0,0 +1,21 @@
+import smtplib
+
+GMAIL_USER = 'your.address@gmail.com'
+GMAIL_PASS = 'your password'
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PORT = 587
+
+def send_email(recipient, subject, text):
+    smtpserver = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+    smtpserver.ehlo()
+    smtpserver.starttls()
+    smtpserver.ehlo
+    smtpserver.login(GMAIL_USER, GMAIL_PASS)
+    header = 'To:' + recipient + '\n' + 'From: ' + GMAIL_USER
+    header = header + '\n' + 'Subject:' + subject + '\n'
+    msg = header + '\n' + text + ' \n\n'
+    smtpserver.sendmail(GMAIL_USER, recipient, msg)
+    smtpserver.close()
+
+send_email('recipient@email.address', 'sub', 'this is text')
+print "sent"
