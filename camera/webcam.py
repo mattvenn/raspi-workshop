98e5e58c76c9b784685b2cfd76ec430e95ee9be6
Matt Venn <matt@mattvenn.net>
2013-09-28 13:26:25 +0100

    made it a bit simpler

--- camera/webcam.py
+++ camera/webcam.py
@@ -6,7 +6,7 @@ author: matt venn
 import os
 
 #this function takes a photo using the fswebcam command
-def capture_image(filename="frame.jpg"):
+def capture_image(filename):
     #this can do banners and timestamps
     command = "fswebcam  --no-banner -r 800x600 -d /dev/video0 %s" % filename
     #command = "fswebcam --set brightness=50%% --no-banner -r 800x600 -d /dev/video0 %s" % filename
@@ -15,4 +15,4 @@ def capture_image(filename="frame.jpg"):
 
 
 #take a photo
-capture_image()
+capture_image('picture.jpg')
