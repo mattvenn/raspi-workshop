295fb3d5eae24ea4e80722e8c4491de8c722687a
Matt Venn <matt@mattvenn.net>
2013-09-30 15:41:30 +0100

    removed %s and changed comment

--- camera/webcam.py
+++ camera/webcam.py
@@ -7,9 +7,8 @@ import os
 
 #this function takes a photo using the fswebcam command
 def capture_image(filename):
-    #this can do banners and timestamps
-    command = "fswebcam  --no-banner -r 800x600 -d /dev/video0 %s" % filename
-    #command = "fswebcam --set brightness=50%% --no-banner -r 800x600 -d /dev/video0 %s" % filename
+    #the fswebcam command can also do banners and timestamps
+    command = "fswebcam  --no-banner -r 800x600 -d /dev/video0 " + filename
     status = os.system(command)
     return status
 
