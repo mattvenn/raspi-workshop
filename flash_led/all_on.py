2d92b1770dfae78b89673950abb8ed645a7a6d37
Matt Venn <matt@mattvenn.net>
2013-09-30 19:30:29 +0100

    turn all outputs on

---
+++ flash_led/all_on.py
@@ -0,0 +1,15 @@
+#import the library to control the GPIO pins
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+#a list of all the pins
+pins = [ 3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 23, 24, 26 ]
+
+#turn on all pins
+for pin in pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, True)
+
+#cleanup
+GPIO.cleanup()
