a50f80fdc3029d1751d8846bb3f6a42e4f8223a0
Matt Venn <matt@mattvenn.net>
2013-10-14 19:23:17 +0100

    fix indentation

--- button/button.py
+++ button/button.py
@@ -4,7 +4,6 @@ author: matt venn
 
 #import the library
 import RPi.GPIO as GPIO
-GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
 button_pin = 8
@@ -14,4 +13,6 @@ GPIO.setup(button_pin,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
     if GPIO.input(button_pin) == False:
-	print "button pressed"
+        print "button pressed"
+
+
